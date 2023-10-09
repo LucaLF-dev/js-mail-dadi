41e737b93d159b16c53287d85204fab351833abb
LucaLF-dev <laficaraluca@gmail.com>
2023-10-09 18:08:47 +0200

    Added - dadi js to app js

--- js/app.js
+++ js/app.js
@@ -1,21 +1,21 @@
 // - chiediamo l' email all' utente
-const emailUtente = prompt(' scrivi qui la tua email')
-console.log(emailUtente)
+const emailUtente = prompt(' scrivi qui la tua email');
+// console.log(emailUtente)
 
 // - creaiamo una variabile array con la lista delle email
-const emailsUtenti = ['mamma@gmail.com', 'marta@gmail.com', 'polo@gmail.com', 'festa@gmail.com', 'pollo@gmail.com'] 
-console.log(emailsUtenti)
+const emailsUtenti = ['mamma@gmail.com', 'marta@gmail.com', 'polo@gmail.com', 'festa@gmail.com', 'pollo@gmail.com']; 
+// console.log(emailsUtenti)
 
 let isEmailFound = false; // variabile booleano
 //   - confrontiamo l'email dell utente con l'email della lista
-const lunghezzaArray = emailsUtenti.length
+const lunghezzaArray = emailsUtenti.length;
 for (let i = 0; i < lunghezzaArray; i++)  {
-    const emailsCorrenti = emailsUtenti[i]
-    console.log(emailsCorrenti)
+    const emailsCorrenti = emailsUtenti[i];
+    // console.log(emailsCorrenti)
 
 // - stampiamo un messaggio appropriato
     if (emailsCorrenti === emailUtente) {
-        isEmailFound = true
+        isEmailFound = true;
 //   - SE l'email è presente:
 //     - accesso confermato
     
@@ -24,9 +24,29 @@ for (let i = 0; i < lunghezzaArray; i++)  {
 } }
 
 if (isEmailFound === true) {
-    console.log('accesso consentito')
+    console.log('accesso consentito');
 
 } else {
-    console.log('accesso negato') 
+    console.log('accesso negato'); 
 }
-console.log(isEmailFound)
+
+// - creo una variabile per il giocatore:
+//    - genero un numero random da 1 a 6
+const playerNumber = Math.floor(Math.random() * (6 - 1) + 1); 
+console.log(playerNumber);
+
+// - creo una variabile per il computer:
+//    - genero un numero random da 1 a 6
+const computerNumber = Math.floor(Math.random() * (6 - 1) + 1); 
+console.log(computerNumber);
+
+// - stabilire il vincitore, in base a chi fa il punteggio piu alto
+let winnerGame 
+
+if (playerNumber > computerNumber) {
+    winnerGame = ' Player Win';
+} else {
+    winnerGame = ' Computer Win'
+}
+
+console.log(winnerGame)
